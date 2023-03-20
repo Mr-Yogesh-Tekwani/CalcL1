3cd9acc75ed3fdcbb1440ed6e694c18f3fba2db0
YOGESH TEKWANI <61420707+Mr-Yogesh-Tekwani@users.noreply.github.com>
2023-03-20 16:07:35 -0400

    Add files via upload

---
+++ CalcWithoutStoryboard/CalcWithoutStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalcWithoutStoryboard
+//
+//  Created by Yogesh Tekwani on 3/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
